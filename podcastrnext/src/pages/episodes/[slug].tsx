50e47941efdc3accd83d66fca13a81563120b61e
VictorAugustoqwe <vaugustocruz22@gmail.com>
2021-04-25 23:03:15 -0300

    nlw fim

--- podcastrnext/src/pages/episodes/[slug].tsx
+++ podcastrnext/src/pages/episodes/[slug].tsx
@@ -3,10 +3,12 @@ import { api } from '../../services/api';
 import ptBR from 'date-fns/locale/pt-BR'
 import { format, parseISO } from 'date-fns';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
+import Head from 'next/head'
 import Image from 'next/image';
 import Link from 'next/link';
 
 import styles from './episode.module.scss';
+import { usePlayer } from '../../contexts/PlayerContext';
 
 type Episode = {
   id: string;
@@ -26,9 +28,12 @@ type EpisodeProps = {
 
 
 export default function Episode({ episode }: EpisodeProps) {
-
+  const { play } = usePlayer()
   return (
     <div className={styles.episode}>
+      <Head>
+        <title>{episode.title} | Podcastr</title>
+      </Head>
       <div className={styles.thumbnailContainer}>
         <Link href="/">
           <button type="button" >
@@ -42,7 +47,7 @@ export default function Episode({ episode }: EpisodeProps) {
           objectFit="cover">
         </Image>
 
-        <button type="button" >
+        <button type="button" onClick={() => play(episode)} >
           <img src="/play.svg" alt="Tocar episódio" />
         </button>
       </div>
